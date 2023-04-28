9f2a902c339f8e057b74ad2c33f3841146e3e65e
JJmojica23 <jjmojica2015@gmail.com>
2023-04-28 11:43:27 -0700

    Finished the project and added the bonus features
    The project has been finished and added the bonus features except for expert.

--- Prototype5MojicaP3/Assets/Scripts/GameManager.cs
+++ Prototype5MojicaP3/Assets/Scripts/GameManager.cs
@@ -12,22 +12,41 @@ public class GameManager : MonoBehaviour
     private int score;
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI gameOverText;
+    public TextMeshProUGUI livesText;
     public bool isGameActive;
     public Button restartButton;
+    public GameObject titleScreen;
+    public GameObject pausedScreen;
+    private int lives;
+    public bool isGamePaused = false;
     
 
     // Start is called before the first frame update
     void Start()
     {
+    
+    }
+
+    public void StartGame(int difficulty)
+    {
         isGameActive = true;
         StartCoroutine(SpawnTarget());
         score = 0;
+        lives = 3;
         UpdateScore(0);
+        UpdateLives(0);
+        titleScreen.gameObject.SetActive(false);
+        spawnRate /= difficulty;
+        isGamePaused = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (isGameActive == true)
+        {
+            pauseGame();
+        }
         
     }
 
@@ -47,15 +66,48 @@ public class GameManager : MonoBehaviour
         scoreText.text = "Score: " + score;
     }
 
+    public void UpdateLives(int livesLeft)
+    {
+        lives -= livesLeft;
+        livesText.text = "Lives: " + lives;
+    }
+
     public void GameOver()
     {
-        isGameActive = false;
-        gameOverText.gameObject.SetActive(true);
-        restartButton.gameObject.SetActive(true);
+        if (lives <= 0)
+        {
+            isGameActive = false;
+            gameOverText.gameObject.SetActive(true);
+            restartButton.gameObject.SetActive(true);
+        }
+        else
+        {
+            UpdateLives(+1);
+        }
     }
 
     public void RestartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void pauseGame()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isGamePaused = !isGamePaused;
+            if (isGamePaused)
+            {
+                pausedScreen.gameObject.SetActive(true);
+                Time.timeScale = 0;
+                AudioListener.pause = true;
+            }
+            else
+            {
+                pausedScreen.gameObject.SetActive(false);
+                Time.timeScale = 1;
+                AudioListener.pause = false;
+            }
+        }
     }
 }
